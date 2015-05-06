316885d94debc7da46aa9e94804a27dd605c5515
KKPmedia <KKP.game@gmx.de>
2015-05-06 17:35:54 +0200

    ih

---
+++ Assets/_scripts/DinoRed_Scripts/AttackScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttackScript : MonoBehaviour {
+
+
+	Animator anim; 
+	Follow follow;
+
+	public float dino_red_bite_damage = 2f;
+
+	// Use this for initialization
+	void Start () {
+		anim = this.GetComponent<Animator> ();
+		follow = this.GetComponent<Follow> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D (Collider2D col) {
+		if (col.CompareTag ("attack_area")) {
+			follow.enabled = false;
+			anim.SetBool("attack", true);
+			//Invoke ("setAttackFalse", 0.5f);
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D col) {
+		follow.enabled = true;
+		anim.SetBool ("attack", false);
+	}
+
+	void setAttackFalse() {
+		follow.enabled = true;
+		anim.SetBool ("attack", false);
+	}
+
+	public bool isAttack () {
+		if (anim.GetBool ("dead"))
+			return false;
+		return anim.GetBool ("attack");
+	}
+}
---
+++ Assets/_scripts/Player_Scripts/DestroyShoot.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyShoot : MonoBehaviour {
+
+	Renderer re;
+
+	// Use this for initialization
+	void Start () {
+		re = this.GetComponent<Renderer> ();
+	}
+	// Update is called once per frame
+	void Update () {
+		if (re.isVisible == false) 
+			Destroy (gameObject, 0f);
+	}
+}
