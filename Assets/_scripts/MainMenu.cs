d8cdfa3cfd29426389cc0ea2914df8ec627a65db
KKPmedia <KKP.game@gmx.de>
2015-05-11 12:22:38 +0200

    gtf

---
+++ Assets/_scripts/MainMenu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour 
+{
+	public void ChangeToScene (int sceneToChangeTo){
+		Application.LoadLevel (sceneToChangeTo);
+	}
+
+
+}
