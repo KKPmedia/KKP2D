397fec4bc5a430661e52e3b46af2396ef7202a6d
KKPmedia <KKP.game@gmx.de>
2015-05-26 12:08:09 +0200

    bguv v v czt zt ct

--- Assets/_scripts/MainMenu.cs
+++ Assets/_scripts/MainMenu.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class MainMenu : MonoBehaviour 
 {
-
+	public GameObject nomuted;
 	public void ChangeToScene (int sceneToChangeTo){
 		if (sceneToChangeTo > 12) {
 			Application.Quit ();
@@ -12,7 +12,27 @@ public class MainMenu : MonoBehaviour
 				Time.timeScale = 1;
 			Application.LoadLevel (sceneToChangeTo);
 		}
+	
+		if (sceneToChangeTo == 99) {
+			AudioListener.volume = 0;
+			nomuted.SetActive (false);
+		}
+		if (sceneToChangeTo == 100) {
+			AudioListener.volume = 1;
+			nomuted.SetActive (true);
+		}
 	}
+
+	void Start(){
+		if (AudioListener.volume == 1) {
+			nomuted.SetActive (true);
+		} else {
+			nomuted.SetActive (false);
+		}
+
+
+	}
+
 
 
 
